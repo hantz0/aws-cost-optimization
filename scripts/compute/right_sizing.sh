ac85252a5de424f8001f44637daab8ed3f157023
hantz0 <hantz@diversi.cloud>
2024-10-16 15:47:51 -0400

    Update right_sizing.sh

--- scripts/compute/right_sizing.sh
+++ scripts/compute/right_sizing.sh
@@ -1,1 +1,9 @@
 
+#!/bin/bash
+# Script: right_sizing.sh
+# Description: Retrieve EC2 instance right-sizing recommendations.
+
+# Get recommendations for EC2 instances
+aws compute-optimizer get-ec2-instance-recommendations \
+    --query 'instanceRecommendations[*].{InstanceId:instanceId, CurrentType:currentInstanceType, RecommendedType:recommendationOptions[0].instanceType}' \
+    --output table
